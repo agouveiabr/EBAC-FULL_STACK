7b8b9e5eab4000d7bfe468dcc1cbd5ed89da392a
Alvaro Gouveia <40841706+agouveiabr@users.noreply.github.com>
2022-05-13 16:47:28 -0300

    Exercicio - Mod. 19

--- E-Commerce/js/custom.js
+++ E-Commerce/js/custom.js
@@ -1,62 +1,37 @@
-// instancia jquery e evita conflitos
-// jQuery( function($){
-$(document).ready(function(){
+$(".botao-form").click(function (e) {
+  var nome = $("#nome").val();
+  var email = $("#email").val();
+  var cpf = $("#cpf").val();
+  var erro = false;
+  var msg = "";
+  if (nome.length <= 2) {
+    $("#nome").addClass("invalid");
+    msg = "Preencha o campo nome";
+    erro = true;
+  } else {
+    $("#nome").removeClass("invalid");
+  }
 
-    $('.owl-carousel').owlCarousel();
+  if (!email.includes("@") || !email.includes(".")) {
+    $("#email").addClass("invalid");
+    msg = "Email inválido";
+    erro = true;
+  } else {
+    $("#email").removeClass("invalid");
+  }
 
-    let titulos = $('h4') // tag
-   
-    let itens = $('.featured-item') // class
-    
-    let destaques = $('#featured') // id
+  if (!/^\d{3}[\.]?\d{3}[\.]?\d{3}[\-]?\d{2}$/.test(cpf)) {
+    $("#cpf").addClass("invalid");
+    msg = "CPF inválido";
+    erro = true;
+  } else {
+    $("#cpf").removeClass("invalid");
+  }
 
-    console.log(titulos.first());
-
-    // Configuração de produtos
-
-    $('.featured-item a').addClass('btn btn-dark stretch-link');
-
-    $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>')
-    // $('.featured-item:first h4').start('<span class="badge bg-secondary">Novo</span>')
-    // $('.featured-item:first h4').html('<span class="badge bg-secondary">Novo</span>')
-    // $('.featured-item:first h4').addClass('active')
-    // $('.featured-item:first h4').removeClass('active')
-    // $('.featured-item:first h4').toggleClass('active')
-    // $('.featured-item:first h4').hide()
-    // $('.featured-item:first h4').show()
-    // $('.featured-item:first h4').fadeIn(2000)
-    // $('.featured-item:first h4').fadeOut()
-    //  $('.featured-item:first h4').css('color', '#f00')
-     
-     $('.featured-item h4').dblclick( function(){
-
-        $(this).css({
-            'color': '#f00',
-            'background': '#ff0',
-            'font-weight': '100',
-        });
-
-     });
-
-     /*
-      * Manipulação de eventos
-      */
-     $('.featured-item a').on('blur', function(event){
-
-        event.preventDefault();
-
-        alert('Produto esgotado');
-
-     })
-     
-     
-     
-     
-
-
-
-
-
-
-
-})
+  if (erro) {
+    e.preventDefault();
+    alert(msg);
+    return false;
+  }
+  return true;
+});
